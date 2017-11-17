9fcd6c5a811a21203f06839099579a366aefcf50
IKEDA, Yoshifumi <ikd.ysfm@gmail.com>
2017-11-18 02:20:07 +0900

    modify config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+# 2.3未満なら必要な記述
+for file in ~/.config/fish/conf.d/*.fish
+  source $file
+end
+
 # fish_user_pathsも使えるが、プロビジョニングで扱いづらいので環境変数はここに定義
 # -gを付けなくても位置的にグローバルだが明示的に
 set -gx GOPATH $HOME/.go
