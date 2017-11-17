bcccb0917ee2156bd79d1659a3ad4827e5b3c4c7
IKEDA, Yoshifumi <ikd.ysfm@gmail.com>
2017-11-18 01:03:41 +0900

    modify config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,4 @@
-set GOPATH $HOME/.go
-set PATH $PATH $GOPATH/bin
+# fish_user_pathsも使えるが、プロビジョニングで扱いづらいので環境変数はここに定義
+# -gを付けなくても位置的にグローバルだが明示的に
+set -gx GOPATH $HOME/.go
+set -gx PATH $PATH $GOPATH/bin
