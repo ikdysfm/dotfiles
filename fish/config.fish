45ab86f7b7a46e22856740baf2733922f4ba3a8b
IKEDA, Yoshifumi <ikd.ysfm@gmail.com>
2017-12-03 02:05:41 +0900

    modify tmux and fish settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,10 @@
 # fish_user_pathsも使えるが、プロビジョニングで扱いづらいので環境変数はここに定義
-# -gを付けなくても位置的にグローバルだが明示的に
-set -gx GOPATH $HOME/.go
-set -gx PATH $PATH $GOPATH/bin
-set -gx EDITOR /usr/bin/vi
+# tmuxやサブシェル起動時にも読み込まれるためその都度PATHが長くならないようにフラグ制御
+if [ -z $LOADED ]
+  set -x GOPATH $HOME/.go
+  set -x PATH $PATH $GOPATH/bin
+  set -x EDITOR /usr/bin/vi
+end
 
 # キーバインド定義
 function fish_user_key_bindings
@@ -19,12 +21,11 @@ bass source ~/.nvm/nvm.sh
 balias g git
 balias tmux "direnv exec / tmux" # tmux起動前にdirenvを一旦unload
 
-# 最初の一回だけ実行。サブシェル起動時も除外されるようにexportする
-if [ -z "$SMART_ATTACHED" ]
-  set -gx SMART_ATTACHED y
-  tmux_smart_attach
-end
-
 # direnv
 eval (direnv hook fish)
 
+# 最初の一回だけ実行。サブシェル起動時も除外されるようにexportする
+if [ -z $LOADED ]
+  set -x LOADED y
+  tmux_smart_attach
+end
--- fish/functions/tmux_smart_attach.fish
+++ fish/functions/tmux_smart_attach.fish
@@ -1,7 +1,7 @@
 function tmux_smart_attach
-  if [ -z "$TMUX" ] # ログインシェルかどうかはとりあえず見ない
+  if [ -z $TMUX ] # ログインシェルかどうかはとりあえず見ない
     set -l sessions (tmux list-sessions)
-    if [ -z "$sessions" ]  # セッションがなければ新しく作る
+    if [ -z $sessions ]  # セッションがなければ新しく作る
       tmux new-session
       return
     end
@@ -10,10 +10,10 @@ function tmux_smart_attach
     set -l list $sessions "$new_session":
     set -l choice (echo -s {$list}\n | peco | cut -d: -f1)
     
-    if [ "$choice" = "$new_session" ]
+    if [ $choice = $new_session ]
       tmux new-session
-    else if [ -n "$choice" ]
-      tmux attach-session -t "$choice"
+    else if [ -n $choice ]
+      tmux attach-session -t $choice
     end
     # 何も選択されなかった場合は普通に端末を表示する
   end
