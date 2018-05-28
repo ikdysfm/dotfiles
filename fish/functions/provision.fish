076a8abae7d893f627265f37605dcba2b50f2986
IKEDA, Yoshifumi <ikd.ysfm@gmail.com>
2018-05-28 21:44:55 +0900

    Modified provision.fish

--- fish/functions/provision.fish
+++ fish/functions/provision.fish
@@ -1,3 +1,3 @@
 function provision
-  env ANSIBLE_CONFIG=/vagrant/provisioning/ansible.cfg ansible-playbook /vagrant/provisioning/playbook.yml -i /vagrant/provisioning/hosts
+  ansible-playbook ~/src/github.com/ikdysfm/ansible-ubuntu/localhost.yml -i ~/src/github.com/ikdysfm/ansible-ubuntu/hosts --ask-become-pass
 end
