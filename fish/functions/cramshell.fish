62989b1ade27bbb2057a756a2cec124d5a618eeb
IKEDA, Yoshifumi <ikd.ysfm@gmail.com>
2018-06-23 21:57:11 +0900

    cramshellコマンドを追加

---
+++ fish/functions/cramshell.fish
@@ -0,0 +1,11 @@
+function cramshell
+  # 接続するたびに変わる可能性があるのでその時の値を取得
+  set number (cat /proc/bus/input/devices | grep -A 4 'HHKB Pro' | grep -o 'event[0-9]\+')
+
+  if test -n "$number"
+    sudo -b xkeysnail /home/yoshifumi/src/github.com/ikdysfm/dotfiles/xkeysnail/hhkb_types.py -- devices /dev/input/"$number" > /dev/null 2>&1
+  end
+
+  # .xsessionrcに書いてあるのと同じ
+  xinput set-button-map 'Kensington Kensington Slimblade Trackball' 1 8 3 4 5 6 7 9
+end
